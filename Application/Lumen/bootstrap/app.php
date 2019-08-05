2c4845230ff3eb64482000d2e329be09ec4a2fb7
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2019-08-05 17:27:12 +0100

    staging

--- Application/Lumen/bootstrap/app.php
+++ Application/Lumen/bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -102,7 +102,7 @@ $app->register(Appzcoder\LumenRoutesList\RoutesCommandServiceProvider::class);
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__ . '/../routes/web.php';
+    require __DIR__ . '/../../../routes/web.php';
 });
 
 $app->configure('audit');
